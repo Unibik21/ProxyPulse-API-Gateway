2b82a827528a3787102d91734b6e989d0663f7cd
Unibik21 <utkarshcha21@gmail.com>
2026-08-16 00:05:07 +0530

    Redis setup

--- gateway/src/config.ts
+++ gateway/src/config.ts
@@ -4,8 +4,13 @@ export const config = {
 <<<<<<< HEAD
   port: Number(process.env.PORT) || 7000,
   controlPlaneUrl: process.env.CONTROL_PLANE_URL || "http://localhost:3000",
+<<<<<<< HEAD
 =======
     port: Number(process.env.PORT) || 7000,
     controlPlaneUrl: process.env.CONTROL_PLANE_URL || 'http://localhost:3000',
 >>>>>>> f1b54c6 (Correcting the mistakes)
+=======
+  redisUrl: process.env.REDIS_URL || 'redis://localhost:6379',
+>>>>>>> fff5556 (Redis setup)
 };
+
