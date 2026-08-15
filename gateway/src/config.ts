9baa1389295315260155ef0a0a84b88f49984359
Unibik21 <utkarshcha21@gmail.com>
2026-08-15 21:23:06 +0530

    Correcting the mistakes

--- gateway/src/config.ts
+++ gateway/src/config.ts
@@ -1,6 +1,11 @@
 import "dotenv/config";
 
 export const config = {
+<<<<<<< HEAD
   port: Number(process.env.PORT) || 7000,
   controlPlaneUrl: process.env.CONTROL_PLANE_URL || "http://localhost:3000",
+=======
+    port: Number(process.env.PORT) || 7000,
+    controlPlaneUrl: process.env.CONTROL_PLANE_URL || 'http://localhost:3000',
+>>>>>>> f1b54c6 (Correcting the mistakes)
 };
