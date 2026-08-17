6c953d0d715fb6f2f351848c680da446bafbbce4
arhantsg07 <sfarhant098@gmail.com>
2026-08-17 17:59:24 +0530

    deleted generated js fiiles

--- gateway/src/redis.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.redis = void 0;
-var ioredis_1 = require("ioredis");
-var config_js_1 = require("./config.js");
-exports.redis = new ioredis_1.Redis(config_js_1.config.redisUrl);
-exports.redis.on('error', function (err) { return console.error('Redis error: ', err); });
