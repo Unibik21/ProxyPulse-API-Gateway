ee2a48b24d59e2a8c25edaae8c7a7107d3031959
arhantsg07 <sfarhant098@gmail.com>
2026-08-22 16:07:10 +0530

    minor change

--- gateway/src/healthCheck.ts
+++ gateway/src/healthCheck.ts
@@ -5,13 +5,12 @@ export async function checkServiceHealth(){
     const services = await getAllServices();
     for(const svc of services){
         try{
-            const res = await fetch (`${svc.baseUrl}/health`,{signal:AbortSignal.timeout(2000)});
+            const res = await fetch(`${svc.baseUrl}/health`,{signal:AbortSignal.timeout(2000)});
             await redis.set(`service_health:${svc.name}`, res.ok ? 'healthy': 'unhealthy','EX',30);
 
         }
         catch{
             await redis.set(`service_name:${svc.name}`,'unreachable','EX',30);
-
         }
     }
 }
