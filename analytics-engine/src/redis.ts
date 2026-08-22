d8f4babe2c59d15460ab0070f876d5409af64e95
arhantsg07 <sfarhant098@gmail.com>
2026-08-22 18:45:23 +0530

    import fix

--- analytics-engine/src/redis.ts
+++ analytics-engine/src/redis.ts
@@ -1,5 +1,5 @@
 import Redis from 'ioredis';
-import { config } from './config';
+import { config } from './config.js';
 
 export const redis = new Redis(config.redisUrl);
 export const blockingRedis = new Redis(config.redisUrl)
