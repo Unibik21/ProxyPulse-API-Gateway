af3b8cfffec3d754720a7a2d4e15a1990bbb6557
arhantsg07 <sfarhant098@gmail.com>
2026-08-22 16:06:52 +0530

    analytical engine

---
+++ analytics-engine/src/redis.ts
@@ -0,0 +1,8 @@
+import Redis from 'ioredis';
+import { config } from './config';
+
+export const redis = new Redis(config.redisUrl);
+export const blockingRedis = new Redis(config.redisUrl)
+
+redis.on('error', (err:Error) => console.error('Redis error: ', err));
+blockingRedis.on('error', (err:Error) => console.error('Blocking redis error: ', err))
